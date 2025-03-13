93a4375fce735dcf9c7362de9f3fadc086b8fb0f
DavidRubioMoreno <darubi04@ucm.es>
2025-03-13 13:44:57 +0100

    metodo extra de camara para scar info constantemente

--- Assets/Scripts/Player/CameraRaycast.cs
+++ Assets/Scripts/Player/CameraRaycast.cs
@@ -10,18 +10,32 @@ public class CameraRaycast : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
             shootRay();
+
+        shootInfoRay();
     }
 
-    void shootRay()
+    void shootRay()//Metodo para recoger el recolectable
     {
         Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
         RaycastHit hit;
 
-        Debug.DrawRay(ray.origin, ray.direction * maxDistance, Color.red, 1.0f);
+        //Debug.DrawRay(ray.origin, ray.direction * maxDistance, Color.red, 1.0f);
 
         if (Physics.Raycast(ray, out hit, maxDistance))
         {
             GameManager.Instance.objectDetectedRaycast(hit.collider.gameObject);
+            Debug.Log("Objeto golpeado: " + hit.collider.gameObject.name);
+        }
+    }
+
+    
+    void shootInfoRay()//Metodo para marcar la mira como que se ha detectado un recolectable
+    {
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, maxDistance))
+        {
             Debug.Log("Objeto golpeado: " + hit.collider.gameObject.name);
         }
     }
