072abb6ad1bd894cab1c869fea946cba0775a96c
Mikeey84 <milope21@ucm.es>
2025-03-14 17:34:27 +0100

    Animacion de pegar no se buggea

--- Assets/Scripts/Player/AutoAttack.cs
+++ Assets/Scripts/Player/AutoAttack.cs
@@ -8,6 +8,13 @@ public class AutoAttack : MonoBehaviour
     [Header("Collider")]
     [SerializeField]
     private GameObject _collider;
+
+    [Header("Attack properties")]
+    [SerializeField]
+    private int _damage;
+    [SerializeField]
+    private float _reloadTime;
+
     private Animator _animator;
     private int _random;
     private bool _canAttack;
@@ -30,13 +37,15 @@ public class AutoAttack : MonoBehaviour
 
     public void Attack(InputAction.CallbackContext callback)
     {
-        if (_canAttack && _myPM.IsGrounded()){
+        if (_canAttack && _myPM.IsGrounded())
+        {
             _random = Random.Range(0, 2);
             if (_random == 0)
                 _animator.SetBool("Attack", true);
             else
                 _animator.SetBool("Attack2", true);
             _canAttack = false;
+            Debug.Log("ATTACK");
         }
     }
 
@@ -51,10 +60,15 @@ public class AutoAttack : MonoBehaviour
 
     public void AcabaAnimacion()
     {
-        if (_random == 0)
-            _animator.SetBool("Attack", false);
-        else
-            _animator.SetBool("Attack2", false);
+        _animator.SetBool("Attack", false);
+        _animator.SetBool("Attack2", false);
+
+        StartCoroutine(WaitForReloadTime());
+        Debug.Log("ACABA");
+    }
+    IEnumerator WaitForReloadTime()
+    {
+        yield return new WaitForSeconds(_reloadTime);
         _canAttack = true;
     }
 }
