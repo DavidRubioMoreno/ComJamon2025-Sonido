3f2b058c3cb7ac15a5388ae4bbec02211fef0cfb
[MarcosPRC] <marcopan@ucm.es>
2025-03-15 15:46:18 +0100

    menu

--- Assets/Scripts/OptionMenu.cs
+++ Assets/Scripts/OptionMenu.cs
@@ -2,23 +2,26 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Security.Cryptography;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
     public GameObject optionsMenu; // El panel del menú de opciones
     public GameObject exitConfirmationPanel; // El panel de confirmación de salida
-    public Slider sliderGeneral;
-    public Slider sliderMusic;
-
+    //public Slider sliderGeneral;
+    //public Slider sliderMusic;
+    [SerializeField] private AudioMixer audioMixer; // Mixer principal
+    [SerializeField] private Slider volumenSlider; // Slider para volumen general
+    [SerializeField] private Slider musicaSlider; // Slider para música
     private void Start()
     {
         // Cargar valores guardados
-        sliderGeneral.value = PlayerPrefs.GetFloat("GeneralVolume", 1f);
-        sliderMusic.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        volumenSlider.value = PlayerPrefs.GetFloat("GeneralVolume", 1f);
+        musicaSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
 
         // Asignar los valores al AudioListener
-        sliderGeneral.onValueChanged.AddListener(ChangeGeneralVolume);
-        sliderMusic.onValueChanged.AddListener(ChangeMusicVolume);
+        volumenSlider.onValueChanged.AddListener(ChangeGeneralVolume);
+        musicaSlider.onValueChanged.AddListener(ChangeMusicVolume);
     }
 
     public void OpenOptions()
@@ -33,15 +36,14 @@ public class OptionsMenu : MonoBehaviour
 
     public void ChangeGeneralVolume(float value)
     {
-        AudioListener.volume = value;
-        PlayerPrefs.SetFloat("GeneralVolume", value);
+        audioMixer.SetFloat("Musica", Mathf.Log10(value) * 20); // Ajustar volumen
+        PlayerPrefs.SetFloat("SFX", value); // Guardar configuración
     }
 
     public void ChangeMusicVolume(float value)
     {
-        // Suponiendo que tienes un objeto con AudioSource para la música
-        GameObject.Find("MusicPlayer").GetComponent<AudioSource>().volume = value;
-        PlayerPrefs.SetFloat("MusicVolume", value);
+        audioMixer.SetFloat("SFX", Mathf.Log10(value) * 20); // Ajustar música
+        PlayerPrefs.SetFloat("Musica", value); // Guardar configuración
     }
 
     public void ShowExitConfirmation()
