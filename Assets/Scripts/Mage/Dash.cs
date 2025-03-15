26c9a099f4113b7c991a7ee03b6b30eb4602bc11
SergioPerezRobledano <sepere12@ucm.es>
2025-03-15 14:14:27 +0100

    Dash

---
+++ Assets/Scripts/Mage/Dash.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Dash : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float force;
+
+    private float _dash;
+
+    private int nDash;
+
+    private Rigidbody _rb;
+    private Animator _an;
+    private PlayerMovement _pm;
+    void Start()
+    {
+        _rb=GetComponent<Rigidbody>();
+        _an=GetComponent<Animator>();
+        _pm=GetComponent<PlayerMovement>();
+        nDash = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_pm.IsGrounded())
+        {
+            nDash = 0;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (_dash > 0)
+        {
+            _dash = 0;
+            _rb.AddForce(transform.forward.normalized *  force,ForceMode.Impulse);
+            _an.SetBool("Dash", true);
+            nDash += 1;
+        }
+    }
+    public void EndDash()
+    {
+        _an.SetBool("Dash", false);
+    }
+    public void Dashito(InputAction.CallbackContext callback)
+    {
+        Debug.Log("Dash");
+        if (!_an.GetBool("Dash")&&nDash<1)
+        {
+            _dash = callback.ReadValue<float>();
+        }
+        
+    }
+}
