283e7cff88cc1f2193d1bb6439804e467260f034
DavidRubioMoreno <darubi04@ucm.es>
2025-03-15 19:53:51 +0100

    rama importante

--- Assets/Scripts/Recolectables/Branch.cs
+++ Assets/Scripts/Recolectables/Branch.cs
@@ -5,15 +5,11 @@ using UnityEngine;
 public class Branch : Collectable
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     public override void onCollect() 
     {
         GameManager.Instance.addBranch();
-        Destroy(gameObject);
+        Destroy(transform.parent.gameObject);
     }
 }
